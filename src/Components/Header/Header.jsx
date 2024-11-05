969c1d3173eea36086bfdccd72044a2f486d4c42
Gonzalo Nicolás Nazzetta <nazzettagonzalo@gmail.com>
2024-11-05 19:26:48 -0300

    Creación de la estructura y de las pantallas

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <div>
+            <div className="logo-container">
+                <img id="logo de la app" src={logo-fitnow.jpeg}></img>
+            </div>    
+        </div>
+    )
+}
+
+export default Header;
